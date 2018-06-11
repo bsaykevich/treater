3962bb2418c4839fd37dd0f8dafe002fabb6ff03
bsaykevich <bsaykevich@gmail.com>
2018-06-11 11:30:22 +0300

    postgre added

---
+++ src/main/java/com/example/treater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.treater.repos;
+
+import com.example.treater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
