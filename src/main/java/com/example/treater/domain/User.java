37a71a9ae60386207b9724c598c0af7cb2c35189
bsaykevich <bsaykevich@gmail.com>
2018-06-17 00:05:15 +0300

    email account registration confirmation added

--- src/main/java/com/example/treater/domain/User.java
+++ src/main/java/com/example/treater/domain/User.java
@@ -25,6 +25,9 @@ public class User implements UserDetails{
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    private String email;
+    private String activationCode;
+
     public boolean isAdmin(){
         return roles.contains(Role.ADMIN);
     }
@@ -69,6 +72,21 @@ public class User implements UserDetails{
         this.roles = roles;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
